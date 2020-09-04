934f918b8417cdcca555a820cd56e96a1fda4c32
Alexandre Debusschère <zizilex@gmail.com>
2020-09-04 19:20:35 +0700

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import '@shopify/polaris/dist/styles.css';
+import Navigation from './Navigation';
+import fr from './locales/fr.json';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+i18next
+    .use(initReactI18next)
+    .init({
+        debug: false,
+        resources: {
+            fr: fr
+        },
+        lng: 'en',
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false
+        },
+        keySeparator: false,
+        nsSeparator: false
+    })
+    .then(() => {
+        ReactDOM.render(
+            <React.StrictMode>
+                <Navigation/>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+    });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
